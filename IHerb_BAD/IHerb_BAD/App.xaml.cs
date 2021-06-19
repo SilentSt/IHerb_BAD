073ffe31eb24c4087c20e93548938ae5338ac165
minaxciS <67161810+4Whiskas@users.noreply.github.com>
2021-06-19 14:49:13 +0300

    AnalysPage ready

--- IHerb_BAD/IHerb_BAD/App.xaml.cs
+++ IHerb_BAD/IHerb_BAD/App.xaml.cs
@@ -14,7 +14,7 @@ namespace IHerb_BAD
             //MainPage = new NavigationPage(new Profile());
 
 
-            MainPage = new NavigationPage(new Profile());
+            MainPage = new NavigationPage(new Analysis());
 
 
         }
---
+++ IHerb_BAD/IHerb_BAD/MVVM/View/Analysis.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IHerb_BAD.MVVM.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Analysis : ContentPage
+    {
+        public Analysis()
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
