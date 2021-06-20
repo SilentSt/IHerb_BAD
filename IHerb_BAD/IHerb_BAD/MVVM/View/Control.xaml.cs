a24ab388d6904a55afd1ae23a077933fd015dd57
minaxciS <67161810+4Whiskas@users.noreply.github.com>
2021-06-20 03:49:15 +0300

    ControlPage

---
+++ IHerb_BAD/IHerb_BAD/MVVM/View/Control.xaml.cs
@@ -0,0 +1,22 @@
+﻿using IHerb_BAD.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IHerb_BAD.MVVM.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Control : ContentPage
+    {
+        public Control()
+        {
+            InitializeComponent();
+            BindingContext = new ControlViewModel();
+        }
+    }
+}
---
+++ IHerb_BAD/IHerb_BAD/MVVM/ViewModel/ControlViewModel.cs
@@ -0,0 +1,34 @@
+﻿using IHerb_BAD.MVVM.Core;
+using Syncfusion.XForms.Pickers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IHerb_BAD.MVVM.ViewModel
+{
+    class ControlViewModel:ObservableObject
+    {
+        public ICommand ShowDatePicker { get; set; }
+        public string selDate { get; set; } = "< Выберите дату >";
+        public ControlViewModel()
+        {
+            ShowDatePicker = new Command(OpenDatePicker);
+        }
+        public void OpenDatePicker(object sender)
+        {            
+            var date = (sender as SfDatePicker);
+            date.IsOpen = true;
+            date.DateSelected += Date_DateSelected;
+            
+        }
+
+        private void Date_DateSelected(object sender, Syncfusion.XForms.Pickers.DateChangedEventArgs e)
+        {
+            var date = (sender as SfDatePicker).Date;
+            selDate = "< "+date.DayOfWeek.ToString()+", "+date.Day.ToString()+" "+date.Month.ToString()+" >";
+            Fire("selDate");
+        }
+    }
+}
